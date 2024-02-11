2a34081f7ea3e533d72c946bcf6dcd624d093573
DannyHwang <DannyHwang@DESKTOP-8OGLLLF>
2024-02-11 18:17:09 +0900

    update

---
+++ src/tree/BJ1991.java
@@ -0,0 +1,21 @@
+package tree;
+
+import java.util.ArrayList;
+
+public class BJ1991 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Bnode> al = new ArrayList<Bnode>();
+		
+		
+		
+	}
+
+}
+
+class Bnode {
+	char name;
+	Bnode leftChild;
+	Bnode rightChild;
+}
