40e6eec3e4917189295d9e841ba87a5d34c85fb5
Danny <Danny@DannyPC>
2024-02-11 17:49:56 +0900

    initial revison

---
+++ src/codingtest/chapter05/CT68936.java
@@ -0,0 +1,10 @@
+package codingtest.chapter05;
+
+public class CT68936 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/codingtest/chapter05/CT84512.java
@@ -0,0 +1,23 @@
+package codingtest.chapter05;
+
+public class CT84512 {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		
+		String word = "AAAAE";
+		solution.solution(word);
+	}
+}
+
+class Solution {
+    public int solution(String word) {
+    	
+    	char[] item = {'A','E','I','O','U'};
+    	
+    	System.out.println(item[0]);
+    	
+        int answer = 0;
+        return answer;
+    }
+}
